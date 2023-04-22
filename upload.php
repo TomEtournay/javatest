c2123efb51a12b3ba370a538b70e9031fc1931f3
Tom Etournay <89395881+TomEtournay@users.noreply.github.com>
2023-04-22 11:56:37 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 if(isset($_FILES['file']) && $_FILES['file']['error'] == 0) {
     $name = $_FILES['file']['name'];
     $tmp_name = $_FILES['file']['tmp_name'];
-    $target_dir = "upload/";
+    $target_dir = "/upload/";
     $target_file = $target_dir . basename($name);
 
     if(move_uploaded_file($tmp_name, $target_file)) {
